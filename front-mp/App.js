175403ae78c27b68f173693583a346c651427fc6
David Mendes <davidgmendes@hotmail.com>
2021-05-17 14:22:48 -0300

    Login e Cadastro funcionando pelo front!

--- front-mp/App.js
+++ front-mp/App.js
@@ -4,14 +4,16 @@ import { View } from 'react-native';
 import Signin from './pages/Signin';
 import { ButtonSubmit, CadastroSubmit, TextSubmit } from './pages/Signin/styles';
 import {TextInput,container} from 'react-native';
+import { api } from './services/api';
 
 
 
 export default function App() {
 
-const [nome, setNome] = useState('');
-const [email, setEmail] = useState('');
-const [senha, setSenha] = useState('');
+const [nome, setNome] = useState([]);
+const [email, setEmail] = useState([]);
+const [senha, setSenha] = useState([]);
+const [confirmacaoSenha, setConfirmacaoSenha] = useState([]);
 
   return (
     
@@ -23,24 +25,22 @@ const [senha, setSenha] = useState('');
       <CadastroSubmit>
         <ButtonSubmit>
         <TextSubmit>
-          Cadastro ------>  ------>  
+        <div onClick={() => {api.post('create', {
+                                            name: nome, email: email,
+                                            password: senha, password_confirmation: confirmacaoSenha
+                                          }).then(response => console.log(response))}}>
+            Cadastro:
+        </div>
         </TextSubmit>
         </ButtonSubmit>
 
         <TextInput placeholder="Seu nome" style={StyleSheet.TextInput}onChangeText={text=>setNome(text)}/>
         <TextInput placeholder="Seu E-mail" style={StyleSheet.TextInput}onChangeText={text=>setEmail(text)}/>
         <TextInput secureTextEntry = {true}placeholder="Cria uma senha" style={StyleSheet.TextInput}onChangeText={text=>setSenha(text)}/>
-        <TextInput secureTextEntry = {true}placeholder="confirme sua senha" style={StyleSheet.TextInput}onChangeText={text=>setSenha(text)}/>
-
+        <TextInput secureTextEntry = {true}placeholder="confirme sua senha" style={StyleSheet.TextInput}onChangeText={text=>setConfirmacaoSenha(text)}/>
 
       </CadastroSubmit>
-
-
-
-
-
     </>
-
   );
 
 
--- front-mp/pages/Signin/index.js
+++ front-mp/pages/Signin/index.js
@@ -1,11 +1,15 @@
-import React from 'react'; 
+import React, { useState, useEffect } from 'react'; 
 import {KeyboardView, Title,Container,Input,ButtonSubmit,TextSubmit} from './styles';  
 import Header from '../../componets/Header';
+import {api} from '../../services/api';
+import axios from 'axios';
 /*
 import { Container } from '../../componets/Header/styles';
 import { Button } from 'react-native';
 */
 function Signin(){
+    const [email, setEmail] = useState([]);
+    const [senha, setSenha] = useState([]);
     return(
         <KeyboardView>
             <Header/>
@@ -19,19 +23,19 @@ function Signin(){
             <Input
             placeholderTextColor = "#fff"
             placeholder = "Senha"
-            /* onChangeText={text=>setSenha(text)} */
+            onChangeText={text=>setSenha(text)}
             secureTextEntry
             />
             <ButtonSubmit>
-             <TextSubmit>Entrar</TextSubmit>
+                <div onClick={() => {api.post('sign_in', {email: email, password: senha})
+                                                .then(response => console.log(response))}}>
+                {/* <button onClick={() => {api.get('quizzes/index')
+                                            .then(response => console.log(response))}}> */}
+                <TextSubmit>Entrar</TextSubmit>
+                </div>
             </ButtonSubmit>
             </Container>
         </KeyboardView>
-        
-
-
-
-
     )
 }
 
