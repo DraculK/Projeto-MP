143c10761e3446834fd60d5f51fbdb5a1762866a
David Mendes <davidgmendes@hotmail.com>
2021-05-19 15:33:18 -0300

    corrigindo a besteira q o rubocop mandou eu fazer

--- final-mp/app/models/alternative.rb
+++ final-mp/app/models/alternative.rb
@@ -1,7 +1,7 @@
 class Alternative < ApplicationRecord
   belongs_to :question
   validates :body, presence: true
-  validate :body, uniqueness: { scope: :question_id, message: '- Alternativa repetida!' }
+  validates :body, uniqueness: { scope: :question_id, message: '- Alternativa repetida!' }
   validates :correct_answer,
             inclusion: { in: [true, false], scope: :question_id, if: :gradeble_question?,
                          message: '- Essa questão vale nota. Informe se a alternativa está correta ou não!' }
